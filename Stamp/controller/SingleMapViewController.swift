5b9ecac53157e13c646801d1c80fa076b0905bd2
halawata13 <rsuzuki13@gmail.com>
2018-04-09 21:48:04 +0900

    Push up the screen by the height of the keyboard when lifting the keyboard

--- Stamp/controller/SingleMapViewController.swift
+++ Stamp/controller/SingleMapViewController.swift
@@ -8,6 +8,7 @@ import MapKit
 
 final class SingleMapViewController: UIViewController, MapSnapshotShare, AlertDialog {
     var location: Location?
+    var keyboardHeight: CGFloat?
 
     @IBOutlet private weak var mapView: SingleLocationMapView!
     @IBOutlet private weak var locationInfoView: LocationInfoView!
@@ -42,6 +43,21 @@ final class SingleMapViewController: UIViewController, MapSnapshotShare, AlertDi
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(onTapShareButton))
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // タイトル入力時にキーボードの高さ分画面を押し上げるための監視を追加
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // タイトル入力時にキーボードの高さ分画面を押し上げるための監視を削除
+        NotificationCenter.default.removeObserver(self)
+    }
+
     @objc func tapView() {
         // 画面をタップしたときにキーボードを下げる
         locationInfoView.titleTextField.resignFirstResponder()
@@ -49,6 +65,29 @@ final class SingleMapViewController: UIViewController, MapSnapshotShare, AlertDi
 
     @objc func onTapShareButton() {
         shareMapSnapshot(region: mapView.region)
+    }
+
+    @objc func keyboardWillShow(notification: Notification?) {
+        // キーボードを上げたときにキーボードの高さ分画面を押し上げる
+        guard let rect = (notification?.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+
+        keyboardHeight = keyboardHeight ?? rect.size.height - (tabBarController?.tabBar.frame.size.height ?? 0)
+
+        let duration = notification?.userInfo?[UIKeyboardAnimationDurationUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!, animations: { [weak self] in
+            let transform = CGAffineTransform(translationX: 0, y: -(self?.keyboardHeight)!)
+            self?.view.transform = transform
+        })
+    }
+
+    @objc func keyboardWillHide(notification: Notification?) {
+        // キーボードが消えたときに画面を戻す
+        let duration = notification?.userInfo?[UIKeyboardAnimationCurveUserInfoKey] as? Double
+        UIView.animate(withDuration: duration!, animations: { [weak self] in
+            self?.view.transform = CGAffineTransform.identity
+        })
     }
 }
 
